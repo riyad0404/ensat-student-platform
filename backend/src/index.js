4c48f29c706b7a44cab2c005f0a873ca5c64e969
Riyad <riyadtheone04@gmail.com>
2025-12-04 14:12:42 +0100

    switched from require to import

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 // src/index.js
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express'
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
