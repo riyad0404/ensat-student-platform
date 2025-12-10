13a70787590aceb6ba3af9dd884e49f44cd92231
oumniya <elabaidoumniya.elabaid.oumniya@etu.uae.ac.ma>
2025-12-10 21:49:56 +0100

    modif

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -76,7 +76,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout, register, loading }}>
+    <AuthContext.Provider value={{ user, login, logout, register, loading, }}>
       {children}
     </AuthContext.Provider>
   );
