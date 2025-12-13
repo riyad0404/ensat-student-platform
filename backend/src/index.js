daf7df5511eb4192e4fe6e0bd6fa3b8f855852a5
nouhailaelmohsine <nouhailaaamohsine@gmail.com>
2025-12-13 18:04:28 +0100

    changement de style + reset password by email and code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
-import express, { urlencoded } from 'express'
-import cors from 'cors';
 import dotenv from 'dotenv';
+dotenv.config();              // ✅ TOUJOURS TOUT EN HAUT
+
+import express from 'express';
+import cors from 'cors';
 import authRoutes from './routes/authroutes.js';
 import sequelize from './database.js';
 import cookieParser from 'cookie-parser';
-dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -32,15 +34,9 @@ app.get('/api/health', (req, res) => {
 
 async function start() {
   try {
-    // 1. Check DB connectivity
     await sequelize.authenticate();
     console.log('✅ Database connection established.');
 
-    // 2. Sync models → create/alter tables
-    await sequelize.sync({ alter: true });
-    console.log('✅ Models synchronized with database.');
-
-    // 3. Start HTTP server
     app.listen(PORT, () => {
       console.log(`🚀 Server listening on http://localhost:${PORT}`);
     });
