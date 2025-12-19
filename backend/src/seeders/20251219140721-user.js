ef88235628312516ed379e4290edbb9ece7634c0
unknown <l>
2025-12-19 20:22:13 +0100

    search + synchronisation des documents

--- backend/src/seeders/20251219140721-user.js
+++ backend/src/seeders/20251219140721-user.js
@@ -1,73 +1,83 @@
 import bcrypt from "bcrypt";
-import sequelize from "../database.js";
-import { User } from "../models/user.js";
 
-const seedUsers = async () => {
-  try {
-    await sequelize.authenticate();
-    const users = [
-      {
-        nom: "El Ghazrani ",
-        prenom: "Jihane",
-        email: "elghazranijihane@gmail.com",
-        password: await bcrypt.hash("jihane05", 10),
-        niveau: "GINF2",
-        secretCode: 200520,
-        bio: "Étudiant ENSAT – Génie Informatique",
-        photo: null,
-      },
-      {
-        nom: "Moukhliss",
-        prenom: "Riyad",
-        email: "salma.ait@ensat.ma",
-        password: await bcrypt.hash("password123", 10),
-        niveau: "GINF2",
-        secretCode: 200421,
-        bio: "Étudiante ENSAT – Génie Informatique",
-        photo: null,
-      },
-      {
-        nom: "El Ghomari",
-        prenom: "Zohra",
-        email: "hamza.bennani@ensat.ma",
-        password: await bcrypt.hash("password123", 10),
-        niveau: "GINF2",
-        secretCode: 2004211,
-        bio: "Étudiant ENSAT – Première année GI",
-        photo: null,
-      },
-      {
-        nom: "El mohsine",
-        prenom: "Nouhaila",
-        email: "imane.zahraoui@ensat.ma",
-        password: await bcrypt.hash("password123", 10),
-        niveau: "GINF2",
-        secretCode: 20042111,
-        bio: "Étudiante ENSAT – Cycle ingénieur",
-        photo: null,
-      },
-      {
-        nom: "EL Abaid",
-        prenom: "Oumnia",
-        email: "anas.kabbaj@ensat.ma",
-        password: await bcrypt.hash("password123", 10),
-        niveau: "GINF2",
-        secretCode: 2005200,
-        bio: "Étudiant ENSAT – Génie Informatique",
-        photo: null,
-      },
-    ];
+export async function up(queryInterface, Sequelize) {
+  const users = [
+    {
+      nom: "El Ghazrani",
+      prenom: "Jihane",
+      email: "elghazranijihane@gmail.com",
+      password: await bcrypt.hash("jihane05", 10),
+      niveau: "GINF2",
+      secretCode: 200520,
+      bio: "Étudiante ENSAT – Génie Informatique",
+      photo: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      nom: "Moukhliss",
+      prenom: "Riyad",
+      email: "moukhliss.riyad@etu.uae.ac.ma",
+      password: await bcrypt.hash("riyad04", 10),
+      niveau: "GINF2",
+      secretCode: 200421,
+      bio: null,
+      photo: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      nom: "El Ghomari",
+      prenom: "Zohra",
+      email: "elghomarizohra@gmail.com",
+      password: await bcrypt.hash("zohra04", 10),
+      niveau: "GINF2",
+      secretCode: 2004211,
+      bio: null,
+      photo: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      nom: "El Mohsine",
+      prenom: "Nouhaila",
+      email: "elmohsine.nouhaila@etu.uae.ac.ma",
+      password: await bcrypt.hash("password123", 10),
+      niveau: "GINF2",
+      secretCode: 20042111,
+      bio: null,
+      photo: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      nom: "El Abaid",
+      prenom: "Oumnia",
+      email: "elabaid.oumnia@etu.uae.ac.ma",
+      password: await bcrypt.hash("oumnia", 10),
+      niveau: "GINF2",
+      secretCode: 2005200,
+      bio: null,
+      photo: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ];
 
-    await User.bulkCreate(users, {
-      ignoreDuplicates: true, // évite erreur si relancé
-    });
+  await queryInterface.bulkInsert("users", users);
+}
 
-    console.log("✅ 5 étudiants insérés avec succès");
-    process.exit(0);
-  } catch (error) {
-    console.error("❌ Erreur seeder users :", error);
-    process.exit(1);
-  }
-};
-
-seedUsers();
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.bulkDelete(
+    "users",
+    {
+      email: [
+        "elghazranijihane@gmail.com",
+        "moukhliss.riyad@etu.uae.ac.ma",
+        "elghomarizohra@gmail.com",
+        "elmohsine.nouhaila@etu.uae.ac.ma",
+        "elabaid.oumnia@etu.uae.ac.ma",
+      ],
+    }
+  );
+}
