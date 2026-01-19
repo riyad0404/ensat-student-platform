a907d5737a5a5554c974607e6abdaaf86db8fb1b
Jihane El Ghazrani <elghazrani.jihane@etu.uae.ac.ma>
2026-01-19 15:24:24 +0100

    kkkkk

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,17 +1,17 @@
-// src/database.js
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-dotenv.config(); // Charger le fichier .env
+dotenv.config();
 
+// Create Sequelize instance with environment variables
 const sequelize = new Sequelize({
-  username: process.env.DATABASE_USER,       
-  password: process.env.DATABASE_PASSWORD,    
-  database: process.env.DATABASE_NAME,        
-  host: process.env.DATABASE_HOST,            
-  dialect: "postgres",                        
-  logging: false,                             
-  port: process.env.DATABASE_PORT || 5432,    
+  username: process.env.DATABASE_USER ,
+  password: process.env.DATABASE_PASSWORD ,
+  database: process.env.DATABASE_NAME ,
+  host: process.env.DATABASE_HOST ,
+  dialect: 'postgres',
+   logging: false,
+  port: process.env.DATABASE_PORT || 5434,
 });
 
 export default sequelize;
